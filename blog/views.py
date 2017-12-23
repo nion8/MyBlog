a38f7c9f6ceae58236f619670eed32efd135ef55
a.b <nion8@mail.ru>
2017-12-23 20:36:24 +0300

    модель урлы

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import SubscriberForm
 
 
 def blog(request):
     name = "Андрей"
+    current_day = "23.12.2017"
+    from = SubscriberForm(request.POST or None)
     return render(request, 'blog/blog.html', locals())
