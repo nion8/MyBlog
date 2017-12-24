0048bf500883201de8ccd2c9d00e5b43673f76cf
a.b <nion8@mail.ru>
2017-12-24 15:36:56 +0300

    шрифт заголовка

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,13 @@ def blog(request):
     name = "Андрей"
     current_day = "23.12.2017"
     form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+
     return render(request, 'blog/blog.html', locals())
