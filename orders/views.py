21b470b26b7c822f6b24ea0758c64df6bab8d7e5
a.b <nion8@mail.ru>
2018-01-03 07:43:51 +0300

    orders produkt

---
+++ orders/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import SubscriberForm
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def blog(request):
+    name = "Андрей"
+    current_day = "24.12.2017"
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+
+    return render(request, 'blog/blog.html', locals())
---
+++ products/urls.py
@@ -0,0 +1,23 @@
+"""from django.conf.urls import re_path
+from django.contrib import admin
+from blog import views
+
+urlpatterns = [
+    re_path(r'^blog/', views.blog, name='blog'),
+]
+from django.conf.urls import url, include
+from django.contrib import admin
+from blog import views
+
+urlpatterns = [
+    url(r'^blog/', views.blog, name='blog'),
+]"""
+from django.urls import path
+from . import views
+from blog import views
+
+urlpatterns = [
+#    path('', views.home, name='home'),
+    path('', views.blog, name='blog'),
+
+]
