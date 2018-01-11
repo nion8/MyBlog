243a313ac7ee8c706416e92679758a17801ebdd8
a.b <nion8@mail.ru>
2018-01-11 10:25:55 +0300

    user_profael

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from user_profile.models import User
 
 
-class Subscriber(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=128)
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    text = models.CharField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=31, default='Global')
+    is_active = models.BooleanField(default=True)
+    is_favorit = models.BooleanField(default=False)
+
+    # email = models.EmailField()
+    # name = models.CharField(max_length=128)
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+
+
 # Create your models here.
 
 class User(AbstractBaseUser):
     usermname = models.CharField('username', max_length=23, unique=True, db_index=True)
     email = models.EmailField('email adress', unique=True)
-    
+    joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'username '
 
-
-
-
+    def __str__(self):
+        return self.usermname
+    #
+    # def __unicode__(self):
+    #     return self.usermname
